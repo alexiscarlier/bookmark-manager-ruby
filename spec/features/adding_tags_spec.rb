b74809b0847ada7a2dc69a55e3c9916d0b2fed69
Alexis Carlier <alexiscarlier@Alexiss-MacBook.local>
2017-12-21 18:18:55 +0000

    Add ability to filter by multiple tags and tested

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -11,4 +11,14 @@ feature 'Adding tags' do
     expect(link.tags.map(&:name)).to include('altruism')
   end
 
+  scenario 'I can add multiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.twitter.com/'
+    fill_in 'title', with: 'Twitter'
+    fill_in 'tags',  with: 'social media'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('social', 'media')
+  end
+
 end
