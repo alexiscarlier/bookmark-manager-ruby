5b01f6a6955d446a7e00712d61f13313cb957179
Alexis Carlier <alexiscarlier@Alexiss-MacBook.local>
2017-12-21 14:13:04 +0000

    Update Gemfile and spec helper

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 ruby "2.4.1"
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
 
 group :test do
   gem 'rspec'
--- spec/viewing_links_spec.rb
+++ spec/viewing_links_spec.rb
@@ -8,7 +8,7 @@ feature 'Viewing links' do
     expect(page.status_code).to eq 200
 
     within 'ul#links' do
-      expect(page).to have_content('Makers Academy')
+      expect(page).to have_content('Bookmark 1')
     end
   end
 end
